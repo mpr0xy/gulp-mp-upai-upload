22c7bcd2356bca349b2879b4426eb6d60fc1b142
mpr0xy <mpr0xy@live.com>
2015-04-24 14:26:43 +0800

    add prefix

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+var gutil = require('gulp-util'),
+  through = require('through2'),
+  path = require('path'),
+  md5 = require('MD5'),
+  UPYUN = require('upyun'),
+  mime = require('mime'),
+  fs = require('fs'),
+  async = require('async');
+
+module.exports = function(bucket, username, userpass, pathSplit) {
+  var upyun = new UPYUN(bucket, username, userpass, 'v0', 'legacy');
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+    if (file.isStream()) {
+      cb(new gutil.PluginError('gulp-mp-upai-upload', 'Streaming not supported'));
+      return;
+    }
+    var remoteName = file.path.split(pathSplit)[1];
+    remoteName = '/' + pathSplit + remoteName.replace(/\\/g, '/');
+    upyun.uploadFile(remoteName,
+      file.path,
+      mime.lookup(file.path),
+      true, function(err, result) {
+        if (err) {
+          console.error(err);
+          cb(err);
+        } else {
+          console.log("upload success¡¡" +¡¡remoteName);
+          cb();
+        }
+      });
+  });
+}
